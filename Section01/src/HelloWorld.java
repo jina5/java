49514d3601018f5c4239700ef34978e2edbea874
jina <jina@218.38.137.28>
2022-05-05 11:34:09 +0900

    first commit

--- Section01/src/HelloWorld.java
+++ Section01/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) { //코드의 시작은 main
 		System.out.println("Hello World");
 		System.out.println("안녕하세요");
+		System.out.println("안녕!");
 // 순서대로 명령하기 : 절차지향 언어(C) 
 // JAVA : 절차지향 기반에서 확장된 언어
 // 사람언어에 가까운 언어 : 고급언어
