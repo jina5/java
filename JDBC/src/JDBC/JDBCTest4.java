363526a7f9fc9817b72a2888aa187a9a49c51033
Dongsunny <Dongsunnye@naver.com>
2022-05-19 12:06:04 +0900

    class

--- JDBC/src/JDBC/JDBCTest4.java
+++ JDBC/src/JDBC/JDBCTest4.java
@@ -15,14 +15,18 @@ public class JDBCTest4 {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection conn = 
 					DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe","jina","tiger");
-			String sql = "insert into student(sno,snmae,major_no,score) values(?,?,?,?)";
+			String sql = "insert into student(sno, sname,major_no,score) values(?,?,?,?)";
 			PreparedStatement stmt = conn.prepareStatement(sql);
-			stmt.setString(1, "20001111");
-			stmt.setString(2, "홍길동");
-			stmt.setInt(3, 22);
-			stmt.setDouble(4, 2.3);
+			conn.setAutoCommit(false); //자동 커밋 해제
+			stmt.setString(1, "21334232");
+			stmt.setString(2, "김길동");
+			stmt.setInt(3, 13); //setString(3,"22") 해도 된다
+			stmt.setDouble(4, 4.0);
 			
+			int count = stmt.executeUpdate(); //업데이트 횟수
+			System.out.println(count+"건 적용되었습니다.");
 			
+			conn.commit(); //커밋
 			stmt.close();
 			conn.close();
 			
